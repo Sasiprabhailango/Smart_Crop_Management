0370ea99da467bdb3faa14fbcfb790cc61a11a79
Sasiprabhailango <sasiprabha017@gmail.com>
2026-07-05 21:03:02 +0530

    Complete dashboard statistics and crop price management features

--- dashboard.js
+++ dashboard.js
@@ -2,8 +2,10 @@
 const totalCrops = document.getElementById("totalCrops");
 const priceEntries = document.getElementById("priceEntries");
 const highestPrice = document.getElementById("highestPrice");
+const lowestPrice = document.getElementById("lowestPrice");
 const averagePrice = document.getElementById("averagePrice");
 const recentList = document.getElementById("recentList");
+const totalPrice = document.getElementById("totalPrice");
 
 
 let crops = JSON.parse(localStorage.getItem("CropName & price")) || [];
@@ -15,22 +17,68 @@ priceEntries.textContent = crops.length;
 //highest price
 
  let highest =0;
+ let highestCrop = "";
 for(let i =0;i<crops.length;i++){
-    console.log(crops);
-     highest =Math.max(highest,Number(crops[i].price));
+   // console.log(crops);
+     if (Number(crops[i].price) > highest) {
+
+        highest = Number(crops[i].price);
+        highestCrop = crops[i].name;
+
+    }
 
 }
-highestPrice.textContent = "₹" + Number(highest).toLocaleString("en-in");
 
-// average price
+highestPrice.textContent =  highestCrop + " ₹" + Number(highest).toLocaleString("en-in");
+
+//lowest price
+
+ let lowest =crops[0].price;
+ let lowestCrop = "";
+for(let i =0;i<crops.length;i++){
+   // console.log(crops);
+    if (Number(crops[i].price) < lowest) {
+
+        lowest = Number(crops[i].price);
+        lowestCrop = crops[i].name;
+
+    }
+
+}
+lowestPrice.textContent =  lowestCrop +" ₹" + Number(lowest).toLocaleString("en-in");
+
+
+// total value of all crops 
 
 let total = 0;
-let avg =  crops.length;
-for(let i =0 ;i<avg;i++){
+for(let i = 0;i<crops.length;i++){
     total+=Number(crops[i].price);
 }
-let average = (total/avg).toFixed(2);
+totalPrice.textContent = "₹" + Number(total).toLocaleString("en-in");
+
+// average price
+let average =  0;
+if(crops.length > 0){
+    average = (total / crops.length).toFixed(2);
+}else{
+    average = 0;
+}
 averagePrice.textContent = "₹" + Number(average).toLocaleString("en-in");
+
+
+// piechart
+
+const ctx = document.getElementById("priceChart");
+
+let cropName = [];
+let cropPrice = [];
+
+for(let i =0;i<crops.length;i++){
+    cropName.push(crops[i].name);
+    cropPrice.push(crops[i].price);
+}
+console.log(cropName);
+console.log(cropPrice);
 
 // recent list 
 
